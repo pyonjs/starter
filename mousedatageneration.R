a8ae431dd31937e2a03b1b077e7da3a2a1b8c411
James Pyon <pyonjs@hotmail.com>
2018-08-18 15:15:34 -0400

    new data generation method (WIP)

---
+++ mousedatageneration.R
@@ -0,0 +1,96 @@
+
+
+
+ma00 <- rnorm(1, mean = 21.1, sd = 1.5)
+ma01 <- ma00 + 1.8 + rnorm(1, mean = 0, sd = 1)
+ma02 <- ma01 + 1.1 + rnorm(1, mean = 0, sd = 1)
+ma03 <- ma02 + 1.0 + rnorm(1, mean = 0, sd = 1)
+ma0 <- c(ma00, ma01, ma02, ma03)
+
+
+ma10 <- rnorm(1, mean = 21.1, sd = 1.5)
+ma11 <- ma10 + 1.8 + rnorm(1, mean = 0, sd = 1)
+ma12 <- ma11 + 1.1 + rnorm(1, mean = 0, sd = 1)
+ma13 <- ma12 + 1.0 + rnorm(1, mean = 0, sd = 1)
+ma1 <- c(ma10, ma11, ma12, ma13)
+
+
+
+ma20 <- rnorm(1, mean = 21.1, sd = 1.5)
+ma21 <- ma20 + 1.8 + rnorm(1, mean = 0, sd = 1)
+ma22 <- ma21 + 1.1 + rnorm(1, mean = 0, sd = 1)
+ma23 <- ma22 + 1.0 + rnorm(1, mean = 0, sd = 1)
+ma2 <- c(ma20, ma21, ma22, ma23)
+
+ma30 <- rnorm(1, mean = 21.1, sd = 1.5)
+ma31 <- ma30 + 1.8 + rnorm(1, mean = 0, sd = 1)
+ma32 <- ma31 + 1.1 + rnorm(1, mean = 0, sd = 1)
+ma33 <- ma32 + 1.0 + rnorm(1, mean = 0, sd = 1)
+ma3 <- c(ma30, ma31, ma32, ma33)
+
+ma40 <- rnorm(1, mean = 21.1, sd = 1.5)
+ma41 <- ma40 + 1.8 + rnorm(1, mean = 0, sd = 1)
+ma42 <- ma41 + 1.1 + rnorm(1, mean = 0, sd = 1)
+ma43 <- ma42 + 1.0 + rnorm(1, mean = 0, sd = 1)
+ma4 <- c(ma40, ma41, ma42, ma43)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+mb50 <- rnorm(1, mean = 21.1, sd = 1.5)
+mb51 <- ma50 - 2 + 1.8 + rnorm(1, mean = 0, sd = 1)
+mb52 <- ma51 - 2 + 1.8 + rnorm(1, mean = 0, sd = 1)
+mb53 <- ma52 - 2 + 1.8 + rnorm(1, mean = 0, sd = 1)
+
+mb60 <- rnorm(1, mean = 21.1, sd = 1.5)
+mb61 <- ma60 - 2 + 1.8 + rnorm(1, mean = 0, sd = 1)
+mb62 <- ma61 - 2 + 1.8 + rnorm(1, mean = 0, sd = 1)
+mb63 <- ma62 - 2 + 1.8 + rnorm(1, mean = 0, sd = 1)
+
+mb70 <- rnorm(1, mean = 21.1, sd = 1.5)
+mb71 <- ma70 - 2 + 1.8 + rnorm(1, mean = 0, sd = 1)
+mb72 <- ma71 - 2 + 1.8 + rnorm(1, mean = 0, sd = 1)
+mb73 <- ma72 - 2 + 1.8 + rnorm(1, mean = 0, sd = 1)
+
+mb80 <- rnorm(1, mean = 21.1, sd = 1.5)
+mb81 <- ma80 - 2 + 1.8 + rnorm(1, mean = 0, sd = 1)
+mb82 <- ma81 - 2 + 1.8 + rnorm(1, mean = 0, sd = 1)
+mb83 <- ma82 - 2 + 1.8 + rnorm(1, mean = 0, sd = 1)
+
+mb90 <- rnorm(1, mean = 21.1, sd = 1.5)
+mb91 <- ma90 - 2 + 1.8 + rnorm(1, mean = 0, sd = 1)
+mb92 <- ma91 - 2 + 1.8 + rnorm(1, mean = 0, sd = 1)
+mb93 <- ma92 - 2 + 1.8 + rnorm(1, mean = 0, sd = 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
